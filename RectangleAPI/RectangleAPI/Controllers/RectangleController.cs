28e3f8e71361f019b7fafb9fc5581b6a3e65a113
edinm02 <52404195+edinm02@users.noreply.github.com>
2024-04-09 18:49:40 +0200

    Add files via upload

---
+++ RectangleAPI/RectangleAPI/Controllers/RectangleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RectangleAPI.Models;
+using RectangleAPI.Services;
+
+namespace RectangleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class RectangleController : ControllerBase
+    {
+        private readonly RectangleService _rectangleService;
+
+        public RectangleController(RectangleService rectangleService)
+        {
+            _rectangleService = rectangleService;
+        }
+
+        [HttpGet("initial-dimensions")]
+        public async Task<ActionResult<Rectangle>> GetInitialDimensions()
+        {
+            return await _rectangleService.GetInitialDimensionsAsync();
+        }
+
+        [HttpPut("update-dimensions")]
+        public async Task<IActionResult> UpdateDimensions([FromBody] Rectangle dimensions)
+        {
+            if (dimensions == null)
+            {
+                return BadRequest("Invalid dimensions data.");
+            }
+
+            try
+            {             
+                await _rectangleService.UpdateDimensionsAsync(dimensions);
+                return NoContent(); // HTTP 204 No Content
+            }
+            catch (Exception ex)
+            {              
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating dimensions.");
+            }
+        }      
+    }
+}
---
+++ RectangleAPI/RectangleAPI/Services/RectangleService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using RectangleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RectangleAPI.Services
+{
+    public class RectangleService
+    {
+        private readonly string _filePath;
+
+        public RectangleService(IWebHostEnvironment env)
+        {
+            var rootPath = env.ContentRootPath;
+            _filePath = Path.Combine(rootPath, "config.json");
+        }
+
+        public async Task<Rectangle> GetInitialDimensionsAsync()
+        {
+            string jsonData = await File.ReadAllTextAsync(_filePath);
+            return JsonConvert.DeserializeObject<Rectangle>(jsonData);
+        }
+
+        public async Task UpdateDimensionsAsync(Rectangle dimensions)
+        {
+            string jsonData = JsonConvert.SerializeObject(dimensions);
+            await File.WriteAllTextAsync(_filePath, jsonData);
+        }
+    }
+}
